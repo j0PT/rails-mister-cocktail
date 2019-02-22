72f9c6620d0368cce6f2cc1c4abe78bb3a82461c
joanaaperta <jo.aperta@hotmail.com>
2019-02-22 15:49:27 +0000

    want to deployyy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :cocktails, :doses
+  get '/', to: "cocktails#index"
+resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :doses, only: [:new, :create]
+end
 
 #   A user can see the list of cocktails
 # GET "cocktails"
